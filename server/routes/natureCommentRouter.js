b7c539d303d7a8ba0a67c5c90249c515f383cafa
light0617 <light11470617@gmail.com>
2018-09-23 00:20:31 -0700

    build ok

--- server/routes/natureCommentRouter.js
+++ server/routes/natureCommentRouter.js
@@ -19,7 +19,7 @@ NatureCommentRouter.route('/')
       }, (err) => next(err))
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(cors.cors, (req, res, next) => {
     NatureComments.create(req.body)
     .then((comment)=>{
       console.log('Comment Created ', comment);
@@ -28,6 +28,10 @@ NatureCommentRouter.route('/')
       res.json(comment);
     }, (err) => next(err))
     .catch((err) => next(err));
+  })
+  .put(cors.cors, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /comments/');
   })
   .delete((req, res, next) => {
     NatureComments.remove({})
@@ -65,7 +69,7 @@ NatureCommentRouter.route('/')
       .catch((err) => next(err));
   })
   .delete((req, res, next) => {
-    User.findByIdAndRemove(req.params.userId)
+    NatureComments.findByIdAndRemove(req.params.commentId)
     .then((resp) => {
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
